f663fb77500e02be1a42a2c23ea237d6ab20dcec
Giovanni Capuano <webmaster@giovannicapuano.net>
2017-07-29 09:46:38 +0200

    main: follow project conventions

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-const {app, BrowserWindow, crashReporter} = require('electron');
+const { app, BrowserWindow, crashReporter } = require('electron');
 const http = require('http');
 
-crashReporter.start({submitURL: 'http://127.0.0.1:9999', companyName: 'sample'});
+crashReporter.start({ submitURL: 'http://127.0.0.1:9999', companyName: 'sample' });
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// Crash-report collection server
 var server = http.createServer(function(req, res) {
-  // Handle the uploaded crash report from client here
-  // ...
-  // Response the crash report id on server to client.
   res.end(getRandomInt(1000, 9999).toString());
 });
 
