f4834211de1445d2678c750a34f2591785a5a331
Giovanni Capuano <webmaster@giovannicapuano.net>
2017-07-29 13:40:17 +0200

    main: maximize window and comment out console

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 const { app, BrowserWindow, crashReporter } = require('electron');
 const http = require('http');
 
-crashReporter.start({ submitURL: 'http://127.0.0.1:9999', companyName: 'sample' });
+crashReporter.start({
+  submitURL: 'http://127.0.0.1:9999',
+  companyName: 'ginko'
+});
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var server = http.createServer(function(req, res) {
+let server = http.createServer((req, res) => {
   res.end(getRandomInt(1000, 9999).toString());
 });
 
-var mainWindow = null;
+let mainWindow = null;
 app.on('ready', function() {
   server.listen(9999, '127.0.0.1', function () {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadURL('file://' + __dirname + '/build/index.html');
-    mainWindow.toggleDevTools();
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: false
+      }
+    });
+
+    mainWindow.maximize(true);
+    mainWindow.loadURL(`file://${__dirname}/build/index.html`);
+    // mainWindow.toggleDevTools();
   });
 });
